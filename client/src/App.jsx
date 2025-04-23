ed9b6596dc280d8d65e100ba1ce1b665f669ec14
melisamehe <melisamehenktas1@gmail>
2025-04-23 12:15:55 +0300

    Initial commit: Create POS application structure

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import {Header} from "./components/Header/Header";
+import {Categories} from "./components/categories/Categories";
+import React from "react";
+import {Products} from "./components/products/Products";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="home px-6 flex justify-between gap-10">
+         <div className="categories flex-1 overflow-auto max-h-[calc(100vh-_-112px)] pb-64">
+            <Categories />
+         </div>
+         <div className="products flex-[8]">
+          <Products />
+          </div>
+         <div className="">
+           <div>cart totals</div>
+         </div>
+      </div>
+</>
+  );
+}
+
+export default App;
