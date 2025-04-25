a854cd9ce85115a9617ead556e6f25db8bf78032
melisamehenktas1@gmail.com <melisamehenktas1@gmail>
2025-04-25 10:21:21 +0300

    Completed home page layout and responsive design

--- client/src/components/products/Products.jsx
+++ client/src/components/products/Products.jsx
@@ -2,36 +2,96 @@
 
 export const Products = () => {
   return (
-    <div className="products-wrapper grid gap-4"
-    style={{
-      gridTemplateColumns:"repeat(auto-fill,minmax(150px,1fr))"
-    }}>
-        <div className="product-item">
+    <div className="products-wrapper grid grid-cols-card gap-4"
+    >
+        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
            <div className="product-img">
               <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Red_Apple.jpg/1200px-Red_Apple.jpg" alt="" className="h-28 object-cover w-full border-b"
                />
             </div>
-            <div className="product-info">
+            <div className="product-info flex flex-col p-3">
               <span className="font-bold">Elma</span>
               <span>12tl</span>
             </div>
-
-
-
-        </div>
-        <div className="product-item">
+          </div>
+           
+          < div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
            <div className="product-img">
               <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Red_Apple.jpg/1200px-Red_Apple.jpg" alt="" className="h-28 object-cover w-full border-b"
                />
             </div>
-            <div className="product-info">
+            <div className="product-info flex flex-col p-3">
               <span className="font-bold">Elma</span>
               <span>12tl</span>
             </div>
-
-
-
+          
+          
+            
+          
+        
         </div>
+        < div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+           <div className="product-img">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Red_Apple.jpg/1200px-Red_Apple.jpg" alt="" className="h-28 object-cover w-full border-b"
+               />
+            </div>
+            <div className="product-info flex flex-col p-3">
+              <span className="font-bold">Elma</span>
+              <span>12tl</span>
+            </div>
+          
+          
+            
+          
+        
+        </div>
+        < div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+           <div className="product-img">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Red_Apple.jpg/1200px-Red_Apple.jpg" alt="" className="h-28 object-cover w-full border-b"
+               />
+            </div>
+            <div className="product-info flex flex-col p-3">
+              <span className="font-bold">Elma</span>
+              <span>12tl</span>
+            </div>
+          
+          
+            
+          
+        
+        </div>
+        < div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+           <div className="product-img">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Red_Apple.jpg/1200px-Red_Apple.jpg" alt="" className="h-28 object-cover w-full border-b"
+               />
+            </div>
+            <div className="product-info flex flex-col p-3">
+              <span className="font-bold">Elma</span>
+              <span>12tl</span>
+            </div>
+          
+          
+            
+          
+        
+        </div>
+        < div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+           <div className="product-img">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Red_Apple.jpg/1200px-Red_Apple.jpg" alt="" className="h-28 object-cover w-full border-b"
+               />
+            </div>
+            <div className="product-info flex flex-col p-3">
+              <span className="font-bold">Elma</span>
+              <span>12tl</span>
+            </div>
+          
+          
+            
+          
+        
+        </div>
+
+
     </div>
   )
 }
