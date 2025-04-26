366c2bd6ce4508932ce057d47dcbe204758581b9
melisamehenktas1@gmail.com <melisamehenktas1@gmail>
2025-04-27 01:08:34 +0300

    Completed the Cart page with table,total card,and order modal functionality

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,27 @@
+import {Header} from "../components/Header/Header";
+import {Categories} from "../components/categories/Categories";
+import React from "react";
+import {Products} from "../components/products/Products";
+
+import CartTotal from '../components/cart/CartTotal';
+
+const HomePage = () => {
+  return (
+    <>
+    <Header />
+      <div className="home px-6 flex md:flex-row flex-col justify-between gap-10 md:pb-0 pb-24">
+         <div className="categories  overflow-auto max-h-[calc(100vh_-_112px)] md:pb-10 ">
+            <Categories />
+         </div>
+         <div className="products flex-[8] max-h-[calc(100vh_-_112px)] overflow-y-auto pb-10">
+          <Products />
+          </div>
+         <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border ">
+          <CartTotal />
+         </div>
+      </div>
+    </>
+  )
+}
+
+export default HomePage
