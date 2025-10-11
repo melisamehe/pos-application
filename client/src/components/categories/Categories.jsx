4c3e9d57075115ac8b0c92b7df63bb421bdc949b
melisamehenktas1@gmail.com <melisamehenktas1@gmail>
2025-10-11 20:39:48 +0300

    Add category edit and delete functionality

--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -25,7 +25,8 @@ export const Categories = ({categories,setCategories}) => {
         <li className="category-item bg-orange-800 hover:opacity-90" onClick={()=> setIsEditModalOpen(true)}>
           <EditOutlined className="md:text-2xl" />
         </li>
-          <Add isAddModalOpen={isAddModalOpen}
+          <Add 
+          isAddModalOpen={isAddModalOpen}
           setIsAddModalOpen={setIsAddModalOpen}
           categories={categories}
           setCategories={setCategories}
@@ -33,6 +34,8 @@ export const Categories = ({categories,setCategories}) => {
           <Edit 
           isEditModalOpen={isEditModalOpen}
           setIsEditModalOpen={setIsEditModalOpen}
+          categories={categories}
+          setCategories={setCategories}
           />
        
       </ul>
