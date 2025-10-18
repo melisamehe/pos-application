e8a0ff193d0a5d8ed166cfb7ba023d4feacfabbc
melisamehe <melisamehenktas1@gmail.com>
2025-10-18 15:29:08 +0300

    Implemented user registration functionality

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,15 +1,38 @@
-
-import { Button, Carousel, Form, Input } from "antd";
+import { Button, Carousel, Form, Input, message } from "antd";
 import { Link } from "react-router-dom";
 import AuthCarousel from "../../components/auth/AuthCarousel";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Register = () => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+  const onFinish = async (values) => {
+    setLoading(true);
+    try {
+      const res = await fetch("http://localhost:5000/api/auth/register", {
+        method: "POST",
+        body: JSON.stringify(values),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      });
+      if (res.status === 200) {
+        message.success("Kayıt işlemi başarılı.");
+        navigate("/login");
+        setLoading(false);
+      }
+    } catch (error) {
+      message.error("Bir şeyler yanlış gitti.");
+      console.log(error);
+    }
+  };
+
   return (
     <div className="h-screen">
       <div className="flex justify-between h-full">
         <div className="xl:px-20 px-10 w-full flex flex-col h-full justify-center relative">
-          <h1 className="text-center text-5xl font-bold mb-2">MELS STORE</h1>
-          <Form layout="vertical">
+          <h1 className="text-center text-5xl font-bold mb-2">LOGO</h1>
+          <Form layout="vertical" onFinish={onFinish}>
             <Form.Item
               label="Kullanıcı Adı"
               name={"username"}
@@ -55,18 +78,19 @@ const Register = () => {
                   required: true,
                   message: "Şifre Tekrar Alanı Boş Bırakılamaz!",
                 },
-              , ({ getFieldValue }) => ({
+                ({ getFieldValue }) => ({
                   validator(_, value) {
                     if (!value || getFieldValue("password") === value) {
                       return Promise.resolve();
                     }
                     return Promise.reject(
-                      new Error(
-                        "Şifreler Aynı Olmak Zorunda!"
-                      )
+                      new Error("Şifreler Aynı Olmak Zorunda!")
+
+
                     );
                   },
-                }),]}
+                }),
+              ]}
             >
               <Input.Password />
             </Form.Item>
@@ -76,6 +100,7 @@ const Register = () => {
                 htmlType="submit"
                 className="w-full"
                 size="large"
+                loading={loading}
               >
                 Kaydol
               </Button>
@@ -88,7 +113,6 @@ const Register = () => {
             </Link>
           </div>
         </div>
-       
         <div className="xl:w-4/6 lg:w-3/5 md:w-1/2 md:flex hidden bg-[#6c63ff] h-full">
           <div className="w-full h-full flex items-center">
             <div className="w-full">
