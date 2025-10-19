cccf89f08ddc8936205fcf4b3bccad460308d493
melisamehe <melisamehenktas1@gmail.com>
2025-10-19 19:33:19 +0300

    Set API base URL

--- client/src/components/products/Add.jsx
+++ client/src/components/products/Add.jsx
@@ -5,7 +5,7 @@ const Add = ({isAddModalOpen, setIsAddModalOpen,categories,products,setProducts}
   const [form] = Form.useForm();
      const onFinish = (values)=> {
           try{
-            fetch("http://localhost:5000/api/products/add-product",{
+            fetch(process.env.REACT_APP_SERVER_URL + "/api/products/add-product",{
               method:"POST",
               body: JSON.stringify(values),
               headers: {"Content-type":"application/json; charset=UTF-8"},
